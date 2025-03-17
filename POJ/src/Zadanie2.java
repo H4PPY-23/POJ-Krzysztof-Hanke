bb4da7c00753762a413071ca720f5d034445e82c
Krzysztof Hanke <real.h4ppy@pm.me>
2025-03-17 14:18:53 +0100

    zadania 1-3

---
+++ POJ/src/Zadanie2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Zadanie2 {
+    public static void main(String[] args){
+        System.out.println("Podaj stopnie celsjusza");
+        Scanner scanner = new Scanner(System.in);
+        double celsius = scanner.nextDouble();
+        double farenheit = (9/5) * celsius + 32;
+        System.out.println("Stopnie farenheita: " + farenheit);
+    }
+}
---
+++ POJ/src/Zadanie4.java
@@ -0,0 +1,9 @@
+public class Zadanie4 {
+    public static void main(String[] args){
+        System.out.println("Miles   Kilometers");
+        for (int i = 1;i <= 10; i++){
+            double kilo = i * 1.609;
+            System.out.println(i + " " + kilo);
+        }
+    }
+}
